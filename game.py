482c503a64f68b10a258644f1e6245e9d96390a2
Matthew Filmer <Matthew Filmer>
2016-10-20 11:46:55 -0400

    Added some non-functional box2d stuff

--- game.py
+++ game.py
@@ -2,14 +2,29 @@
 # The main worldBuilder game file
 # (c) 2016 Matthew Filmer
 
+# Built in libraries
 import sys
 
+# External libraries
 import pygame
+import Box2D
+
+# Part of this game
 from dwarf import Dwarf
 from block import Wood,Stone,Dirt
 import ai
 
-screen = pygame.display.set_mode((640,480))
+# Lets assume a conversion of 35 pixels/meter
+# This means dwarves are about 2 meters tall
+pxPerM = 35.0           # px/m
+worldWidth = 640        # px
+worldHeight = 480       # px
+world = Box2D.b2World(gravity=(0,-10), doSleep=True)
+groundBody = world.CreateStaticBody( \
+        position=(worldWidth*pxPerM/2,worldHeight*pxPerM+5), \
+        shapes=Box2D.b2PolygonShape(box=(worldWidth*pxPerM,10)))
+
+screen = pygame.display.set_mode((worldWidth,worldHeight))
 dwarfSkin = pygame.image.load('images/dwarf.png').convert()
 dwarfSkin.set_colorkey((255,0,255))
 
